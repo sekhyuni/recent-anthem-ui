4305214a25deddabd2c50196116f04f491609b19
sekhyuni <sekhyuni@gmail.com>
2023-01-29 22:00:03 +0900

    feat: API 테스트용 회원가입 페이지 추가

---
+++ src/pages/join/index.tsx
@@ -0,0 +1,82 @@
+import axios from '~services/index';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+
+import tw from 'twin.macro';
+
+const Join = () => {
+  const queryClient = useQueryClient();
+
+  const { data } = useQuery(
+    ['fetchUser'],
+    () => {
+      return axios.get('/user/kimseokhyun');
+    },
+    {
+      onSuccess: (data) => {},
+    }
+  );
+  const { mutate: createUser } = useMutation(
+    () => {
+      return axios.post('/auth/signup', {
+        userId: 'kimseokhyun',
+        password: 'tprgus920912',
+      });
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(['fetchUser']);
+      },
+    }
+  );
+  const { mutate: updateUser } = useMutation(
+    () => {
+      return axios.put('/user/kimseokhyun', {
+        userId: 'kimseokhyun',
+        password: 'tprgus920912',
+      });
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(['fetchUser']);
+      },
+    }
+  );
+  const { mutate: deleteUser } = useMutation(
+    () => {
+      return axios.delete('/user/kimseokhyun');
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(['fetchUser']);
+      },
+    }
+  );
+  return (
+    <div css={[tw`flex flex-col items-center`]}>
+      <h3>{data?.data?.result?.userId}</h3>
+      <button
+        onClick={() => {
+          createUser();
+        }}
+      >
+        회원가입
+      </button>
+      <button
+        onClick={() => {
+          updateUser();
+        }}
+      >
+        회원정보 수정
+      </button>
+      <button
+        onClick={() => {
+          deleteUser();
+        }}
+      >
+        회원정보 삭제
+      </button>
+    </div>
+  );
+};
+
+export default Join;
