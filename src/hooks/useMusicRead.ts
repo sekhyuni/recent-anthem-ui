894c027c36fad8d5e4859722f669ff7b0eda998e
sekhyuni <sekhyuni@gmail.com>
2023-02-04 23:59:35 +0900

    feat: useMusicQuery.ts 파일명 수정 및 queryKey에 title 추가

--- src/hooks/useMusicRead.ts
+++ src/hooks/useMusicRead.ts
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import * as MusicType from '~types/musicType';
 import MusicService from '~services/musicService';
 
-export const useMusicQuery = (title: MusicType.ListRequestType['title']) => {
+export const useMusicRead = (title: MusicType.ListRequestType['title']) => {
   return useQuery(
-    ['fetchMusic'],
+    ['fetchMusic', title],
     () => {
       const params: MusicType.ListRequestType = {
         title,
