f7cee14c11e2f97f38f755508f392b21f28d4906
sekhyuni <sekhyuni@gmail.com>
2023-02-19 00:55:45 +0900

    fix: Intl.DateTimeFormat 사용 시, hour 표시 형식 h23로 변경

--- src/utils/getAlwaysKoreanCurrentTime.ts
+++ src/utils/getAlwaysKoreanCurrentTime.ts
@@ -5,7 +5,7 @@ const getAlwaysKoreanCurrentTime = (): string => {
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
-    hour12: false,
+    hourCycle: 'h23',
   })
     .format(new Date())
     .replace(/\D/g, '');
